128a043a06c73a25dd99a656e2d894297530d2fa
DanielaOrbes <daniiorbes15@gmail.com>
2024-04-26 10:16:29 -0300

    corrigiendo useEffect

--- src/components/ItemCounts.jsx
+++ src/components/ItemCounts.jsx
@@ -1,27 +1,33 @@
 import { useEffect, useState } from "react"
 
-export const ItemCounts = ({stock, initial, onAdd}) => {
+export const ItemCounts = ({ stock, initial, onAdd }) => {
 
     const [count, setCount] = useState(parseInt(initial));
-    useEffect(() =>{
+
+    useEffect(() => {
         setCount(parseInt(initial));
-   }, [initial]);  // setea el valor inicial
-    
+    }, [initial]);
+
     const decreaseCount = () => {
         setCount(count - 1);
     }
     const increaseCount = () => {
         setCount(count + 1)
     }
+    const addToCart = () => {
+        onAdd(count);
+        setCount(parseInt(initial)); // RESETEA EL COUNT A INITIAL DESPUÉS DE AÑADIR AL CARRITO
+    }
 
     return (
         <div>
-            <button disabled={count>=stock} onClick={increaseCount}>+</button>
+            <button disabled={count >= stock} onClick={increaseCount}>+</button>
             <span>{count}</span>
             <button disabled={count == 0} onClick={decreaseCount}>-</button>
             <div>
-                <button disabled={stock<=1 || count ==0} onClick={() => onAdd(count)}>Agregar al carrito</button>   {/*  onAdd LO EJECUTARA EN EL CONTENEDOR PADRE */}
+                <button disabled={stock <= 1 || count == 0} onClick={addToCart}>Agregar al carrito</button>   {/*  onAdd LO EJECUTARA EN EL CONTENEDOR PADRE */}
             </div>
         </div>
     )
 }
+
